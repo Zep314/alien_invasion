7e8f2b52803890111e88bdfb3735d043842d4124
Dmitry Mordanov <mordanov314@gmail.com>
2022-05-17 17:10:16 +0300

    adding ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,26 @@
 import sys
   
 import pygame
-  
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
  
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
-  
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+              (self.settings.screen_width, self.settings.screen_height))
+        
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
+        
+        # Назначение цвета фона.
+        self.bg_color = (230, 230, 230)
   
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -19,6 +29,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
